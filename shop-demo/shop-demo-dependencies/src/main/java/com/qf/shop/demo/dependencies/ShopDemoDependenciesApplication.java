62f92a7c346108502c18640293d1300ce07c5432
xiaoxiaoMaoi <https://github.com/xiaoxiaoMaoi/nz1905>
2020-03-09 21:34:24 +0800

    增加了项目版本控制

---
+++ shop-demo/shop-demo-dependencies/src/main/java/com/qf/shop/demo/dependencies/ShopDemoDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.qf.shop.demo.dependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDemoDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopDemoDependenciesApplication.class, args);
+    }
+
+}
---
+++ shop-demo/shop-demo-dependencies/src/test/java/com/qf/shop/demo/dependencies/ShopDemoDependenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.shop.demo.dependencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDemoDependenciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

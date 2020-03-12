1a6efb8acf2a36cd7ae701f7f5191ff788062225
Ray.Cheng <840644236@qq.com>
2020-03-12 09:02:50 +0800

    搜索

--- shop-demo/shop-demo-common/common-mapper/src/main/java/com/qf/mapper/TProductDescMapper.java
+++ shop-demo/shop-demo-common/common-mapper/src/main/java/com/qf/mapper/TProductDescMapper.java
@@ -4,18 +4,5 @@ import com.qf.entity.TProductDesc;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface TProductDescMapper extends Mapper<TProductDesc> {
-   /* int deleteByPrimaryKey(Long id);
 
-    int insert(TProductDesc record);
-
-    int insertSelective(TProductDesc record);
-
-    TProductDesc selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(TProductDesc record);
-
-    int updateByPrimaryKeyWithBLOBs(TProductDesc record);
-
-    int updateByPrimaryKey(TProductDesc record);*/
-    //以上请忽略
 }
--- shop-demo/shop-demo-common/common-mapper/src/main/java/com/qf/mapper/TProductMapper.java
+++ shop-demo/shop-demo-common/common-mapper/src/main/java/com/qf/mapper/TProductMapper.java
@@ -1,32 +1,9 @@
 package com.qf.mapper;
 
-<<<<<<< HEAD
-import com.qf.dto.TProductDTO;
 import com.qf.entity.TProduct;
 import tk.mybatis.mapper.common.Mapper;
 
-import java.util.List;
 
 public interface TProductMapper extends Mapper<TProduct> {
-//    int deleteByPrimaryKey(Long pid);
-//
-//    int insert(TProduct record);
-//
-//    int insertSelective(TProduct record);
-//
-//    TProduct selectByPrimaryKey(Long pid);
-//
-//    int updateByPrimaryKeySelective(TProduct record);
-//
-//    int updateByPrimaryKey(TProduct record);
 
-
-    //以上请忽略
-
-=======
-import com.qf.entity.TProduct;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface TProductMapper extends Mapper<TProduct> {
->>>>>>> 4aaafc8f71f28221d738447983d1b0c30126225b
 }

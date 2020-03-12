010d81c104af8e657906c305193fe0cbe9156567
xiaoxiaoMaoi <https://github.com/xiaoxiaoMaoi/nz1905>
2020-03-12 17:11:35 +0800

    处理

---
+++ shop-demo/shop-demo-public-service/back-v2-service/src/main/java/com/qf/back/v2/service/controller/BackController.java
@@ -0,0 +1,14 @@
+package com.qf.back.v2.service.controller;
+
+import com.qf.back.v2.service.service.IBackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("back")
+public class BackController {
+
+    @Autowired
+    private IBackService service;
+}

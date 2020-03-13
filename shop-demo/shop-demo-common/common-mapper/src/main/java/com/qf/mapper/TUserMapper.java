fc2726a676d23ea938330a3ad58017ac195065c8
WangWanxing-Sven <122807775@qq.com>
2020-03-13 16:07:32 +0800

    tUserMapper添加了登录

--- shop-demo/shop-demo-common/common-mapper/src/main/java/com/qf/mapper/TUserMapper.java
+++ shop-demo/shop-demo-common/common-mapper/src/main/java/com/qf/mapper/TUserMapper.java
@@ -4,18 +4,6 @@ import com.qf.entity.TUser;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface TUserMapper extends Mapper<TUser> {
-    int deleteByPrimaryKey(Long id);
-
-    int insert(TUser record);
-
-    int insertSelective(TUser record);
-
-    TUser selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(TUser record);
-
-    int updateByPrimaryKey(TUser record);
 
     TUser selectByUsername(String username);
-    //以上请忽略
 }

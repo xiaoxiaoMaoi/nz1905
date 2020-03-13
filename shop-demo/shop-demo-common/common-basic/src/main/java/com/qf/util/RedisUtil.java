e5144acddcdb5aec864b38247b56e2c2fa55f296
xiaoxiaoMaoi <https://github.com/xiaoxiaoMaoi/nz1905>
2020-03-13 18:55:18 +0800

    购物车bean和依赖处理

--- shop-demo/shop-demo-common/common-basic/src/main/java/com/qf/util/RedisUtil.java
+++ shop-demo/shop-demo-common/common-basic/src/main/java/com/qf/util/RedisUtil.java
@@ -1,7 +1,4 @@
-package com.qf.util;/*
-    @auter wwx
-    @date 2020/3/10
-*/
+package com.qf.util;
 
 public class RedisUtil {
     public static String getRedisKey(String pre,String key){
--- shop-demo/shop-demo-common/common-basic/src/main/java/com/qf/util/StringUtil.java
+++ shop-demo/shop-demo-common/common-basic/src/main/java/com/qf/util/StringUtil.java
@@ -1,7 +1,4 @@
-package com.qf.util;/*
-    @auter wwx
-    @date 2020/3/11
-*/
+package com.qf.util;
 
 public class StringUtil {
 

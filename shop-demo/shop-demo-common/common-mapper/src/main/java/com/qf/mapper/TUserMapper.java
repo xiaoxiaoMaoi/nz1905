f0c3c48178d3dec0d6a9b989cd018a129ef5ca38
xiaoxiaoMaoi <https://github.com/xiaoxiaoMaoi/nz1905>
2020-03-19 22:18:53 +0800

    处理tUser没有登录检测

--- shop-demo/shop-demo-common/common-mapper/src/main/java/com/qf/mapper/TUserMapper.java
+++ shop-demo/shop-demo-common/common-mapper/src/main/java/com/qf/mapper/TUserMapper.java
@@ -1,13 +1,12 @@
 package com.qf.mapper;
 
 import com.qf.entity.TUser;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface TUserMapper extends Mapper<TUser> {
 
-
-
-
+    TUser checkLogin(@Param("username") String username,@Param("password")String password);
 
     TUser selectByUsername(String username);
 }
